c4caf129032859021e0c9dd9345133aa78cca6e0
Romain Lapierre <lapierre.romain@gmail.com>
2017-08-11 15:20:38 -0400

    Add jQuery and condition for availability

--- resources/views/wines/show.blade.php
+++ resources/views/wines/show.blade.php
@@ -9,7 +9,9 @@
 
         <!-- Bootstrap CDN for rapid prototyping !-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
     <body>
     <div class="container">
@@ -46,6 +48,21 @@
         <div class="col-lg-8">
          <h2>{{ $product->prodName }}</h2>
          <h3>${{ $product->prodPriceBuy }}</h3>
+
+         <p>
+           Prod Format : {{ $product->prodFormat }}
+         </p>
+         <p>
+           Buying date : {{ $product->prodDateBuy }}
+         </p>
+
+         <p>
+           @if ($product->prodAvailable === 0)
+              This product is not available for the moment
+          @else
+              This product is available
+          @endif
+         </p>
       </div>
 
       <footer class="footer">
